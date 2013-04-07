59b6c8310b25f999f26897b4cc9b722591161c5b
Kev Jones <kevin@kev-jones.com>
2013-04-07 16:42:57 +0100

    001

---
+++ getstest1.rb
@@ -0,0 +1,8 @@
+puts 'Thank you for your interest please provide you full name;'
+puts first name
+first_name = gets.chomp
+Puts Middle name
+mid_name = gets.chomp
+Puts Last Name
+last_name = gets.chomp
+Puts 'Hi' + first_name + mid_name + last_name + 'how's it hanging?!
---
+++ name.rb
@@ -0,0 +1,4 @@
+name = 'Kevin Richard Jones'
+puts 'my name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
